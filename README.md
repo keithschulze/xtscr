d5ba91925cc7627e1885210d4208ceedc6a88afe
Keith Schulze <keith.schulze@gmail.com>
2015-05-05 14:33:02 +1000

    Initial commit with project layout.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spinal Cord Recovery Analysis in Bitplane Imaris
+Imaris Xtension to automate the analysis of zebrafish spinal cord recovery following injury. In general this xtension requires live confocal or multiphoton images of a nuclei marker where Imaris spots analysis and tracking are using to generate track for the cells through time.
---
+++ xtscr.py
@@ -0,0 +1,24 @@
+#
+#
+#  Spinal Cord Recovery XTension
+#
+#  Copyright (c) 2014 Keith Schulze (keith.schulze@monash.edu)
+#  MIT-style copyright and disclaimer apply
+#
+#    <CustomTools>
+#      <Menu>
+#       <Item name="SCR" icon="Python" tooltip="Spinal Cord Recovery">
+#         <Command>PythonXT::XTSCR(%i)</Command>
+#       </Item>
+#      </Menu>
+#    </CustomTools>
+#
+
+""" Imaris Xtension for the analysis of Spinal Cord Recovery in live
+confocal images.
+"""
+
+def xtscr(appid):
+    """Entry function for the Imaris XTension.
+    """
+    pass
