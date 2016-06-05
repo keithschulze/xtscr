421c69da37e4278fc53976980f6745e0c4a0f2c1
Keith Schulze <keith.schulze@gmail.com>
2016-06-06 08:44:22 +1000

    Shift to using guassian fittings to determine central axis rather than ODR.

--- xtscr.py
+++ xtscr.py
@@ -1,7 +1,7 @@
 #
 #  Spinal Cord Recovery XTension
 #
-#  Copyright (c) 2014 Keith Schulze (keith.schulze@monash.edu)
+#  Copyright (c) 2016 Keith Schulze (keith.schulze@monash.edu)
 #  MIT-style copyright and disclaimer apply
 #
 #    <CustomTools>
@@ -16,16 +16,20 @@
 """ Imaris Xtension for the analysis of Spinal Cord Recovery in live
 confocal images.
 """
-
+import math
+import os
+import time
+import threading
+import Queue
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d as m3d
-import seaborn as sns
-from coord import coord
-from numpy import linalg
 from pIceImarisConnector import pIceImarisConnector as ice
-from scipy.spatial import distance
+from scr import scr
+
+import Tkinter
+import ttk
+import tkFileDialog
+import tkMessageBox
 
 
 __author__ = "Keith Schulze"
@@ -36,213 +40,220 @@ __email__ = "keith.schulze@monash.edu"
 __status__ = "development"
 
 
-def get_lesion_coords(mp):
-    """Get lesion coords from an Imaris measurement points object.
 
-    Parameters
-    ----------
-    mp: MeasurementPoints object
-        Measurement points object from Imaris
+class SCRGUI(object):
 
-    Return
-    ------
-    out: ndarray
-         Numpy array in format [[x, y, z]]
-    """
-    return np.array(mp.GetPositionsXYZ(), np.float)
+    """docstring for SCRGUI"""
+    def __init__(self, master, queue):
+        self.queue = queue
+        self.labeltext = Tkinter.StringVar()
+        self.labeltext.set("SCR Processing")
+        label = Tkinter.Label(master, width=50, textvariable=self.labeltext)
+        self.prog = ttk.Progressbar(master, orient='horizontal', length=300,
+                                    mode='determinate')
+        label.pack(padx=10, pady=10)
+        self.prog.pack(padx=10, pady=10)
 
-
-def get_spots_odr(spot_df):
-    """Determine central axis of the spinal coord by fitting a ODR.
-
-    Parameters
-    ----------
-    spot_df: pandas.DataFrame
-             DataFrame with the columns for x, y and z coordinate for each
-             spot. Also needs a column, time, specifying the time points of
-             each coordinate.
-
-    Return
-    ------
-    line: numpy.ndarray
-          A 3 x 2 numpy array containing 2 3D coords specifying the start and
-          end coords of a ODR line through the spots coordinates.
-    """
-    time0 = spot_df.groupby('time')[["x", "y", "z"]].get_group(0.0).values
-    coord_mean = time0.mean(axis=0)
-
-    # Use SVD to determine first principal component through the mean of the
-    # spot data.
-    _, _, vv = np.linalg.svd(time0 - coord_mean)
-
-    # Estimate the max distance between the two furtherest coords
-    xmin = np.min(spot_df.x)
-    xmax = np.max(spot_df.x)
-    ymin = np.min(spot_df.y)
-    ymax = np.max(spot_df.y)
-    zmin = np.min(spot_df.z)
-    zmax = np.max(spot_df.z)
-    max_dist = distance.cdist(np.array([[xmin, ymin, zmin]]),
-                              np.array([[xmax, ymax, zmax]]))
-    hmd = max_dist[0, 0]/2
-
-    # Get begin and end coords of the line
-    linepts = vv[0] * np.mgrid[-hmd:hmd:2j][:, np.newaxis]
-    linepts += coord_mean
-
-    return linepts
+    def processincoming(self):
+        while self.queue.qsize():
+            try:
+                msg, prog = self.queue.get(0)
+                self.labeltext.set(msg)
+                self.prog.step(prog)
+            except Queue.Empty:
+                pass
 
 
-def project_lc(lesion, line):
-    """Project the estimated lesion coord onto the closest point of a line.
+class SCRProcessor(object):
+    """docstring for SCRProcessor"""
 
-    Parameters
-    ----------
-    lesion: numpy.ndarray
-            A 3 x 1 (x, y, z) numpy array specifying the estimated lesion
-            coordinate.
-    line: numpy.ndarray
-          A 3 x 2 numpy array containing 2 3D coords specifying the start and
-          end coords of a ODR line through the spots coordinates.
+    def __init__(self, master, conn):
+        super(SCRProcessor, self).__init__()
+        self.master = master
+        self.conn = conn
+        self.queue = Queue.Queue()
+        self.gui = SCRGUI(master, self.queue)
+        self.running = True
 
-    Return
-    ------
-    p_lesion: numpy.ndarray
-              A 3 x 1 (x, y, z) numpy array specifying the nearest
-              coordinate along the line to the given lesion coordinate
-              i.e., the projected coordinate.
-    """
-    line_v = np.subtract(line[1], line[0])
-    lesion_v = np.subtract(lesion[0], line[0])
-    proj_lc = (np.dot(lesion_v, line_v)/np.dot(line_v, line_v)) * line_v
+        self.dataset_name, _ = os.path.splitext(
+            os.path.basename(conn.mImarisApplication.GetCurrentFileName())
+        )
 
-    return proj_lc + line[0]
+        dir_options = {'mustexist': True,
+                       'title': 'Please select an output directory'}
+        self.output_dir = tkFileDialog.askdirectory(**dir_options)
+        if len(self.output_dir) == 0:
+            self.master.quit()
+            raise Exception("Open folder cancelled")
+
+        threading.Thread(target=self.workerthread).start()
+        self.periodiccall()
+
+    def periodiccall(self):
+        self.gui.processincoming()
+        if self.running:
+            self.master.after(10, self.periodiccall)
+
+    def extract_sc_centres(self, t):
+        """Extract central axes for a specified time point.
+
+        Parameters
+        ----------
+        t: int
+           timepoint
+
+        Returns
+        -------
+        axes: np.ndarray (float)
+              2 x 3 array storing coords for the start and end coords of
+              the central axis for the given timepoint.
+        """
+        self.queue.put(("Extracting central axis for timepoint: " + str(t), 1))
+        vol = self.conn.getDataVolume(0, t)
+        return scr.extract_sc_centres(vol, 1, slices=50)
+
+
+    def workerthread(self):
+        if not os.path.exists(self.output_dir):
+            print "Folder path does not exist"
+            tkMessageBox.showwarning(
+                "nonexistent folder",
+                "Folder does not exist!"
+            )
+            time.sleep(2)
+            self.master.quit()
+            raise Exception("Folder not found.")
+
+        self.queue.put(("Getting dataset dimensions", 1))
+        # Get dataset parameters
+        xsize, ysize, zsize, csize, tsize = self.conn.getSizes()
+        xvox, yvox, zvox = self.conn.getVoxelSizes()
+        xmin, _, ymin, _, zmin, _ = self.conn.getExtends()
+
+        # Determine central axis in each frame
+        central_axis_cache = os.path.join(self.output_dir,
+                                          self.dataset_name+".npy")
+
+        if os.path.exists(central_axis_cache):
+            self.queue.put(("Extracting central axis for each time point.", 90))
+            sc_centre_coords = np.load(central_axis_cache)
+        else:
+            sc_centre_coords = np.array(
+                [self.extract_sc_centres(t) for t in range(tsize)])
+
+            # Convert to physical coords using voxel sizes
+            sc_centre_coords[:,:,0] = sc_centre_coords[:,:,0] * xvox + xmin
+            sc_centre_coords[:,:,1] = sc_centre_coords[:,:,1] * yvox + ymin
+            sc_centre_coords[:,:,2] = sc_centre_coords[:,:,2] * zvox + zmin
+
+            np.save(central_axis_cache, sc_centre_coords)
+
+        # Get the lesion coords from the measurement points object
+        self.queue.put(("Retrieve lesion coordinates", 1))
+        mp = self.conn.getAllSurpassChildren(False, "MeasurementPoints")[0]
+        if not mp:
+            print "No measurement points marking lesion site specified"
+            tkMessageBox.showerror("No lesion site",
+                                   "Please use measurement points to indicate"
+                                   " lesion site and centre of the notochord"
+                                   " perpendicular to lesion.")
+            self.master.quit()
+            return
+
+        lesion_coords = SCRProcessor.get_lesion_coordinates(mp)
+
+        # Get spot coords from the selected spots object
+        self.queue.put(("Retrieve spot coordinates", 1))
+
+        spots = None
+
+        try:
+            spots = self.conn.getSurpassSelection("Spots")
+        except Exception, e:
+            tkMessageBox.showerror("No Spots object selected",
+                                   "No spots object found or selected.")
+            self.master.quit()
+            return
+
+        if not spots:
+            print "No spots were found. Please select spots object you want"\
+                  "to analyse."
+            tkMessageBox.showerror("No Spots",
+                                   "No spots object founds or not selected."
+                                   " Please select the spots object to want"
+                                   " analyse")
+            self.master.quit()
+            return
+
+        spot_coords = np.array(spots.GetPositionsXYZ(), dtype=np.float)
+
+        # Create a pandas dataframe to summarise data
+        self.queue.put(("Creating dataframe", 1))
+        spot_df = pd.DataFrame(spot_coords.view(dtype=[('x', np.float),
+                                                       ('y', np.float),
+                                                       ('z', np.float)]).ravel())
+
+        # Add tracks to the dataframe
+        spot_df["track"] = np.zeros(len(spot_df["x"]))
+        spot_track_edges = spots.GetTrackEdges()
+        spot_track_ids = spots.GetTrackIds()
+
+        for i, e in zip(spot_track_ids, spot_track_edges):
+            if spot_df.track[e[0]] == 0:
+                spot_df.track[e[0]] = i
+            if spot_df.track[e[1]] == 0:
+                spot_df.track[e[1]] = i
+
+        spot_df.track = spot_df.track.astype(np.int).astype(np.str)
+
+        spot_df['time'] = np.array(spots.GetIndicesT(), np.float) *\
+            self.conn.mImarisApplication.GetDataSet().GetTimePointsDelta()
+
+        # Add time index
+        spot_df['tindex'] = np.array(spots.GetIndicesT())
+
+        # Register to cylindrical coordinates
+        self.queue.put(("Registering on cylindrical coordinate system", 1))
+        spot_df[['r', 'l', 'theta']] = scr.register_cylinder(spot_df, sc_centre_coords,
+                                                             lesion_coords)
+        spot_df['abs_r'] = np.abs(spot_df['r'])
+        spot_df['theta_deg'] = np.rad2deg(np.add(spot_df.theta, np.pi))
+
+        self.queue.put(("Saving dataframe", 1))
+        spot_df.to_csv(os.path.join(self.output_dir, self.dataset_name+".csv"))
+        self.running = False
+        self.master.quit()
+
+    @classmethod
+    def get_lesion_coordinates(cls, measurement_points):
+        """ Get Lesion coordinates from a measurement points object.
+
+        Parameters
+        ----------
+        measurement_points: MeasurementPoints object
+                            MeasurementPoints object from Imaris
+
+        Returns
+        -------
+        lesion_coords: numpy.ndarray
+                       2 x 3 ndarray array representing coordinates for
+                       lesion site and centre of the notochord perpendicular
+                       to the lesion site.
+        """
+        return np.array(measurement_points.GetPositionsXYZ(), np.float)
 
 
 def xtscr(appid):
     """Entry function for the Imaris XTension."""
     conn = ice(appid)
 
-    # Get the lesion coords from the measurement points object
-    mp = conn.getAllSurpassChildren(False, "MeasurementPoints")[0]
-    if not mp:
-        print "No measurement points marking lesion site specified"
+    if not conn.isAlive():
+        print "Could not connect to Imaris"
+        tkMessageBox.showwarning("Connection failed",
+                                 "Could not connect to Imaris!")
+        time.sleep(2)
         return
-    ref_points = get_lesion_coords(mp)
 
-    # Get spot coords from the selected spots object
-    spots = conn.getSurpassSelection("Spots")
-    if not spots:
-        print "No spots were found. Please select spots object you want to"\
-              " analyse."
-        return
-    spot_coords = np.array(spots.GetPositionsXYZ(), dtype=np.float)
-
-    # Create a pandas dataframe to summarise data
-    spot_df = pd.DataFrame(spot_coords.view(dtype=[('x', np.float),
-                                                   ('y', np.float),
-                                                   ('z', np.float)]).ravel())
-
-    # Add tracks to the dataframe
-    spot_df["track"] = np.zeros(len(spot_df["x"]))
-    spot_track_edges = spots.GetTrackEdges()
-    spot_track_ids = spots.GetTrackIds()
-
-    for i, e in zip(spot_track_ids, spot_track_edges):
-        if spot_df.track[e[0]] == 0:
-            spot_df.track[e[0]] = i
-        if spot_df.track[e[1]] == 0:
-            spot_df.track[e[1]] = i
-
-    spot_df.track = spot_df.track.astype(np.int).astype(np.str)
-
-    spot_df['time'] = np.array(spots.GetIndicesT(), np.float) *\
-        conn.mImarisApplication.GetDataSet().GetTimePointsDelta()
-
-    # Get coordinates of line along central axis.
-    line_coords = get_spots_odr(spot_df)
-
-    # Project estimated lesion coords onto the central axis line.
-    lesion_coords = project_lc(ref_points, line_coords)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter3D(*spot_coords.T)
-    ax.scatter3D(*ref_points.T, s=300, c=u'r')
-    ax.scatter3D(*lesion_coords, s=300, c=u'g')
-    ax.scatter3D(*line_coords.T, s=50, c=u'g')
-    ax.plot3D(*line_coords.T)
-    ax.set_xlim(0, 260)
-    ax.set_ylim(0, 260)
-    ax.set_zlim(-0, -200)
-    fig.set_facecolor('white')
-    fig.savefig("/Users/skeith/Desktop/cart3d.png", bbox_inches='tight',
-                facecolor=fig.get_facecolor(), transparent=True)
-
-    # Adjust origin of spot, line and lesion coords to be the start line coord.
-    spot_coords = np.subtract(spot_coords, line_coords[0])
-    lesion_coords = np.subtract(np.array([lesion_coords]), line_coords[0])
-    ref_points = np.subtract(ref_points, line_coords[0])
-    line_coords = np.subtract(line_coords, line_coords[0])
-
-    # Switch to spherical coordinate system to estimate the angle of elevation
-    # of the central axis
-    spot_coords = coord.cartesian2spherical(spot_coords)
-    lesion_coords = coord.cartesian2spherical(lesion_coords)
-    ref_points = coord.cartesian2spherical(ref_points)
-    line_coords = coord.cartesian2spherical(line_coords)
-
-    # Adjust all coords by substracting angle of elevation of the best fit line,
-    # thus making central axis run along Z-axis
-    spot_coords[:, 2] = spot_coords[:, 2] - line_coords[1, 2]
-    lesion_coords[:, 2] = lesion_coords[:, 2] - line_coords[1, 2]
-    ref_points[:, 2] = ref_points[:, 2] - line_coords[1, 2]
-    line_coords[:, 2] = line_coords[:, 2] - line_coords[1, 2]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter3D(*coord.spherical2cartesian(spot_coords).T)
-    ax.scatter3D(*coord.spherical2cartesian(lesion_coords).T, s=50, c=u'r')
-    ax.scatter3D(*coord.spherical2cartesian(ref_points).T, s=50, c=u'r')
-    ax.scatter3D(*coord.spherical2cartesian(line_coords).T, s=50, c=u'g')
-    ax.plot3D(*coord.spherical2cartesian(line_coords).T)
-    ax.set_xlim(-100, 100)
-    ax.set_ylim(-100, 100)
-    ax.set_zlim(0, 300)
-    fig.set_facecolor('white')
-    fig.savefig("/Users/skeith/Desktop/sph3d.png", bbox_inches='tight',
-                facecolor=fig.get_facecolor(), transparent=True)
-
-
-    # Switch to cylindrical coordinate system.
-    spot_coords = coord.spherical2cylindrical(spot_coords)
-    lesion_coords = coord.spherical2cylindrical(lesion_coords)
-    ref_points = coord.spherical2cylindrical(ref_points)
-    line_coords = coord.spherical2cylindrical(line_coords)
-
-    # Reset origin to lesion coord.
-    spot_coords[:, 2] = spot_coords[:, 2] - lesion_coords[:, 2]
-    line_coords[:, 2] = line_coords[:, 2] - lesion_coords[:, 2]
-    ref_points[:, 2] = ref_points[:, 2] - lesion_coords[:, 2]
-    lesion_coords[:, 2] = lesion_coords[:, 2] - lesion_coords[:, 2]
-
-    # Reset azimuth angle to that of second reference point
-    spot_coords[:, 1] = spot_coords[:, 1] - ref_points[1, 1]
-    line_coords[:, 1] = line_coords[:, 1] - ref_points[1, 1]
-    lesion_coords[:, 1] = lesion_coords[:, 1] - ref_points[1, 1]
-    ref_points[:, 1] = ref_points[:, 1] - ref_points[1, 1]
-
-    # Output 3D plot of cylindrical registered scr data
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter3D(*coord.cylindrical2cartesian(spot_coords).T)
-    ax.scatter3D(*coord.cylindrical2cartesian(lesion_coords).T, s=50, c=u'r')
-    ax.scatter3D(*coord.cylindrical2cartesian(ref_points).T, s=50, c=u'r')
-    ax.scatter3D(*coord.cylindrical2cartesian(line_coords).T, s=50, c=u'g')
-    ax.plot3D(*coord.cylindrical2cartesian(line_coords).T)
-    ax.set_xlim(-100, 100)
-    ax.set_ylim(-100, 100)
-    ax.set_zlim(-150, 150)
-    fig.set_facecolor('white')
-    fig.savefig("/Users/skeith/Desktop/cyl3d.png", bbox_inches='tight',
-                facecolor=fig.get_facecolor(), transparent=True)
+    root = Tkinter.Tk()
+    client = SCRProcessor(root, conn)
+    root.mainloop()
